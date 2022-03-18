3bbb22a7ee3a7f393f44c539daff9ec32484a3b9
edanurisik <73337938+edanurisik@users.noreply.github.com>
2022-03-18 15:12:13 +0300

    react native first

---
+++ ilk/src/screens/HomeScreen/index.js
@@ -0,0 +1,23 @@
+import React, { } from 'react';
+import {
+    View,
+    Text,
+    SafeAreaView
+} from 'react-native';
+
+
+const Index = () => {
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={{ flex: 1 }}>
+                <Text>vfmklvfl
+
+                </Text>
+            </View>
+        </SafeAreaView>
+    );
+};
+
+
+
+export default Index;
